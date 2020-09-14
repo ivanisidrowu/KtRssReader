60fdc1b756fed424bdd57737fcf52a871fef52d4
Ivan Wu <ivan055111@gmail.com>
2020-09-14 18:07:27 +0800

    Add declaration-site variance to generic types of the parsers.

--- ktRssReader/src/main/java/tw/ktrssreader/parser/ParserBase.kt
+++ ktRssReader/src/main/java/tw/ktrssreader/parser/ParserBase.kt
@@ -2,4 +2,4 @@ package tw.ktrssreader.parser
 
 import tw.ktrssreader.model.channel.RssStandardChannel
 
-internal abstract class ParserBase<T : RssStandardChannel> : Parser<T>
+internal abstract class ParserBase<out T : RssStandardChannel> : Parser<T>
