083ef204b16b1063a5c5e3875fc1a70770b7bb76
Ivan Wu <ivan055111@gmail.com>
2020-09-15 10:01:02 +0800

    Add standard parser and use open class instead of sealed class.

---
+++ ktRssReader/src/main/java/tw/ktrssreader/parser/StandardParser.kt
@@ -0,0 +1,18 @@
+package tw.ktrssreader.parser
+
+import kotlinx.coroutines.flow.Flow
+import tw.ktrssreader.model.channel.RssStandardChannel
+
+internal class StandardParser : ParserBase<RssStandardChannel>() {
+    override suspend fun parseSuspend(xml: String): RssStandardChannel {
+        TODO("Not yet implemented")
+    }
+
+    override fun parse(xml: String): RssStandardChannel {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseFlow(xml: String): Flow<RssStandardChannel> {
+        TODO("Not yet implemented")
+    }
+}
