73a036938e5ad3d735194683555942a03cbcf91f
Ivan Wu <ivan055111@gmail.com>
2020-09-16 14:57:31 +0800

    Remove test data and use suspendCoroutine.

--- ktRssReader/src/main/java/tw/ktrssreader/parser/RssStandardParser.kt
+++ ktRssReader/src/main/java/tw/ktrssreader/parser/RssStandardParser.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.xmlpull.v1.XmlPullParserException
 import tw.ktrssreader.model.channel.RssStandardChannel
+import kotlin.coroutines.suspendCoroutine
 
 class RssStandardParser : ParserBase<RssStandardChannel>() {
     @Throws(XmlPullParserException::class)
@@ -11,7 +12,7 @@ class RssStandardParser : ParserBase<RssStandardChannel>() {
         return parseStandardChannel(xml)
     }
 
-    override suspend fun parseSuspend(xml: String) = parse(xml)
+    override suspend fun parseSuspend(xml: String) = suspendCoroutine<RssStandardChannel> { parse(xml) }
 
     override fun parseFlow(xml: String): Flow<RssStandardChannel> {
         return flow { emit(parse(xml)) }
