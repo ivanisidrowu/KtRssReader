37102d53ed4233c7e23f71ba369926d8877434bc
Ivan Wu <ivan055111@gmail.com>
2020-09-18 12:02:39 +0800

    Remove unused functions.

--- ktRssReader/src/main/java/tw/ktrssreader/parser/RssStandardParser.kt
+++ ktRssReader/src/main/java/tw/ktrssreader/parser/RssStandardParser.kt
@@ -1,20 +1,11 @@
 package tw.ktrssreader.parser
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import org.xmlpull.v1.XmlPullParserException
 import tw.ktrssreader.model.channel.RssStandardChannel
-import kotlin.coroutines.suspendCoroutine
 
 class RssStandardParser : ParserBase<RssStandardChannel>() {
     @Throws(XmlPullParserException::class)
     override fun parse(xml: String): RssStandardChannel {
         return parseStandardChannel(xml)
-    }
-
-    override suspend fun parseSuspend(xml: String) = suspendCoroutine<RssStandardChannel> { parse(xml) }
-
-    override fun parseFlow(xml: String): Flow<RssStandardChannel> {
-        return flow { emit(parse(xml)) }
     }
 }
