f4f8a097668311468fcdf9fcbf99ec4b61a04bdc
Joseph Wang <dev.weiqi@gmail.com>
2020-09-18 16:43:35 +0800

    Add missed setApplicationContext function

--- ktRssReader/src/main/java/tw/ktrssreader/config/KtRssReaderGlobalConfig.kt
+++ ktRssReader/src/main/java/tw/ktrssreader/config/KtRssReaderGlobalConfig.kt
@@ -6,6 +6,10 @@ object KtRssReaderGlobalConfig {
     private var applicationContext: Context? = null
     var enableLog: Boolean = false
 
+    fun setApplicationContext(applicationContext: Context) {
+        this.applicationContext = applicationContext
+    }
+
     fun getApplicationContext(): Context {
         return applicationContext
             ?: error("No Application Context configured. Please use readerGlobalConfig() DSL.")
