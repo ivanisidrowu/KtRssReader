7ae3e3a49019aca12bb1f23f934df22a7f09a77e
Ivan <ivan055111@gmail.com>
2020-09-23 12:30:54 +0800

    Skip tags if there is unexpected tag inside the specific tag. (#19)

--- ktRssReader/src/main/java/tw/ktrssreader/parser/ParserBase.kt
+++ ktRssReader/src/main/java/tw/ktrssreader/parser/ParserBase.kt
@@ -90,6 +90,11 @@ abstract class ParserBase<out T : RssStandardChannel> : Parser<T> {
         if (next() == XmlPullParser.TEXT) {
             content = text
             nextTag()
+            if (eventType != XmlPullParser.END_TAG) {
+                skip()
+                nextTag()
+                content = null
+            }
         }
         require(XmlPullParser.END_TAG, null, tagName)
         return content
