80cc92614541169c434ee2e4caeffcae5fd4f96b
Ivan <ivan055111@gmail.com>
2021-02-05 11:36:49 +0800

    Add pure Kotlin parser code generators with annotation support. (#66)
    * Add pure Kotlin parser code generators with annotation support.
    
    * Extract the last index from the pre-parsing data.

--- processor/src/main/java/tw/ktrssreader/processor/generator/ExtensionGenerator.kt
+++ processor/src/main/java/tw/ktrssreader/processor/generator/ExtensionGenerator.kt
@@ -16,95 +16,13 @@
 
 package tw.ktrssreader.processor.generator
 
-import com.squareup.kotlinpoet.*
-import tw.ktrssreader.processor.const.*
-import tw.ktrssreader.processor.util.Logger
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.asTypeName
+import tw.ktrssreader.processor.const.METHOD_TO_BOOLEAN
+import tw.ktrssreader.processor.const.TAB
 
-class ExtensionGenerator(
-    private val logger: Logger
-): Generator {
-
-    private val xmlParserClass = ClassName(XML_PULL_PACKAGE, XML_PULL_NAME)
-
-    override fun generate(): FileSpec {
-        return FileSpec.builder(GENERATOR_PACKAGE, EXTENSION_NAME)
-            .addType(
-                TypeSpec.objectBuilder(EXTENSION_NAME)
-                    .addFunction(getXmlParserFunSpec())
-                    .addFunction(getReadStringFunSpec())
-                    .addFunction(getSkipFunSpec())
-                    .addFunction(getBooleanConversionFunSpec())
-                    .build()
-            )
-            .build()
-    }
-
-    private fun getXmlParserFunSpec(): FunSpec {
-        val byteArrayStream = ClassName("java.io", "ByteArrayInputStream")
-        val xmlUtil = ClassName("android.util", "Xml")
-
-        return FunSpec.builder(METHOD_GET_PARSER)
-            .addParameter("xml", String::class)
-            .addCode(
-                """
-                |
-                |%1T(xml.toByteArray()).use { inputStream ->
-                |${TAB}return %2T.newPullParser().apply {
-                |${TAB}${TAB}setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
-                |${TAB}${TAB}setInput(inputStream, null)
-                |${TAB}${TAB}nextTag()
-                |${TAB}}
-                |}
-                |
-                """.trimMargin(),
-                byteArrayStream, xmlUtil
-            )
-            .returns(xmlParserClass)
-            .build()
-    }
-
-    private fun getReadStringFunSpec() = FunSpec.builder(METHOD_READ_STRING)
-        .receiver(xmlParserClass)
-        .addParameter("tagName", String::class)
-        .addCode(
-            """
-            |require(XmlPullParser.START_TAG, null, tagName)
-            |var content: String? = null
-            |if (next() == XmlPullParser.TEXT) {
-            |content = text
-            |nextTag()
-            |${TAB}if (eventType != XmlPullParser.END_TAG) {
-            |${TAB}${TAB}skip()
-            |${TAB}${TAB}nextTag()
-            |${TAB}${TAB}content = null
-            |${TAB}}
-            |}
-            |require(XmlPullParser.END_TAG, null, tagName)
-            |return content
-            """.trimMargin()
-        )
-        .returns(String::class.asTypeName().copy(nullable = true))
-        .build()
-
-    private fun getSkipFunSpec() = FunSpec.builder(METHOD_SKIP)
-        .receiver(xmlParserClass)
-        .addCode(
-            """
-            |if (eventType != XmlPullParser.START_TAG) {
-            |${TAB}throw IllegalStateException()
-            |}
-            |var depth = 1
-            |while (depth != 0) {
-            |${TAB}when (next()) {
-            |${TAB}${TAB}XmlPullParser.END_TAG -> depth--
-            |${TAB}${TAB}XmlPullParser.START_TAG -> depth++
-            |${TAB}}
-            |}
-            """.trimMargin()
-        )
-        .build()
-
-    private fun getBooleanConversionFunSpec() = FunSpec.builder(METHOD_TO_BOOLEAN)
+abstract class ExtensionGenerator : Generator {
+    protected fun getBooleanConversionFunSpec() = FunSpec.builder(METHOD_TO_BOOLEAN)
         .receiver(String::class)
         .addCode(
             """
---
+++ processor/src/main/java/tw/ktrssreader/processor/generator/KotlinExtensionGenerator.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2020 Feng Hsien Hsu, Siao Syuan Yang, Wei-Qi Wang, Ya-Han Tsai, Yu Hao Wu
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package tw.ktrssreader.processor.generator
+
+import com.squareup.kotlinpoet.*
+import org.w3c.dom.Element
+import tw.ktrssreader.processor.const.*
+import tw.ktrssreader.processor.util.Logger
+
+class KotlinExtensionGenerator(
+    private val logger: Logger
+) : ExtensionGenerator() {
+
+    private val elementClass = ClassName("org.w3c.dom", "Element")
+
+    override fun generate(): FileSpec {
+        logger.log("Generating $EXTENSION_NAME for Kotlin.")
+        return FileSpec.builder(GENERATOR_PACKAGE, EXTENSION_NAME)
+            .addType(
+                TypeSpec.objectBuilder(EXTENSION_NAME)
+                    .addFunction(getReadStringFunSpec())
+                    .addFunction(getAttributeOrNullFunSpec())
+                    .addFunction(getElementByTagFunSpec())
+                    .addFunction(getBooleanConversionFunSpec())
+                    .build()
+            )
+            .build()
+    }
+
+    private fun getReadStringFunSpec() = FunSpec.builder(METHOD_READ_STRING)
+        .receiver(elementClass)
+        .addParameter("name", String::class)
+        .addParameter(
+            ParameterSpec
+                .builder("parentTag", String::class.asTypeName().copy(nullable = true))
+                .defaultValue("null")
+                .build()
+        )
+        .addCode(
+            """
+            |val nodeList = getElementsByTagName(name)
+            |if (parentTag == null) {
+            |${TAB}return nodeList.item(0)?.textContent
+            |} else {
+            |${TAB}for (i in 0 until nodeList.length) {
+            |${TAB}${TAB}val e = nodeList.item(i) as? Element ?: continue
+            |${TAB}${TAB}val parent = e.parentNode as? Element
+            |${TAB}${TAB}if (parent?.tagName != parentTag) continue
+            |${TAB}
+            |${TAB}${TAB}return e.textContent
+            |${TAB}}
+            |${TAB}return null
+            |}
+            """.trimMargin()
+        )
+        .returns(String::class.asTypeName().copy(nullable = true))
+        .build()
+
+    private fun getAttributeOrNullFunSpec() = FunSpec.builder(METHOD_GET_ATTR_OR_NULL)
+        .receiver(elementClass)
+        .addParameter("tag", String::class)
+        .addCode(
+            """
+            |val attr = getAttribute(tag) ?: return null
+            |return if (attr.isEmpty() || attr.isBlank()) null else attr
+            """.trimMargin()
+        )
+        .returns(String::class.asTypeName().copy(nullable = true))
+        .build()
+
+    private fun getElementByTagFunSpec() = FunSpec.builder(METHOD_GET_ELEMENT_BY_TAG)
+        .receiver(elementClass)
+        .addParameter("tag", String::class)
+        .addCode(
+            """
+            |val nodeList = getElementsByTagName(tag)
+            |if (nodeList.length == 0) return null
+            |return nodeList.item(0) as? Element 
+            """.trimMargin()
+        )
+        .returns(Element::class.asTypeName().copy(nullable = true))
+        .build()
+}
